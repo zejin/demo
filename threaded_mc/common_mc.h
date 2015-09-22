df80b036e7f8116007b15d654e09782e37592d6b
David Bindel <bindel@cs.cornell.edu>
2015-09-22 13:22:41 -0400

    General refactoring of MC example (push lots of stuff into common_mc.[ch])

---
+++ threaded_mc/common_mc.h
@@ -0,0 +1,37 @@
+#ifndef COMMON_MC_H
+#define COMMON_MC_H
+
+#include "mt19937p.h"
+
+
+/* Parameters for termination criterion */
+typedef struct mc_param_t {
+    int    verbose;    /* Verbosity */
+    double rtol;       /* Relative error bars at convergence */
+    long   maxtrials;  /* Max trials */
+    int    nbatch;     /* Trials per batch */
+} mc_param_t;
+
+
+/* Monte Carlo results */
+typedef struct mc_result_t {
+    double sum_X;
+    double sum_X2;
+    long   ntrials;
+} mc_result_t;
+
+
+void mc_param_init(mc_param_t* param);
+void mc_result_init(mc_result_t* result);
+void mc_result_update(mc_result_t* result, mc_result_t* batch);
+
+void print_params(mc_param_t* param);
+void print_results(mc_result_t* result);
+
+int is_converged(mc_param_t* param, mc_result_t* result);
+
+double run_trial(struct mt19937p* mt);
+void run_trials(struct mt19937p* mt, int nbatch, mc_result_t* result);
+int process_args(int argc, char** argv, mc_param_t* param);
+
+#endif /* COMMON_MC_H */
